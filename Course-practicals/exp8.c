ce901aa90187180d7d0b75eb731abf6a29c6e51d
192524062simats-ops <192524062.simats@saveetha.com>
2026-08-01 09:41:31 +0530

    Create exp8.c

---
+++ Course-practicals/exp8.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+    int bt[20],rem[20],wt[20],tat[20];
+    int n,tq,i,time=0,done;
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Burst Time of P%d: ",i+1);
+        scanf("%d",&bt[i]);
+        rem[i]=bt[i];
+    }
+
+    printf("Enter Time Quantum: ");
+    scanf("%d",&tq);
+
+    do
+    {
+        done=1;
+
+        for(i=0;i<n;i++)
+        {
+            if(rem[i]>0)
+            {
+                done=0;
+
+                if(rem[i]>tq)
+                {
+                    time+=tq;
+                    rem[i]-=tq;
+                }
+                else
+                {
+                    time+=rem[i];
+                    wt[i]=time-bt[i];
+                    rem[i]=0;
+                }
+            }
+        }
+
+    }while(!done);
+
+    printf("\nProcess\tWT\tTAT\n");
+
+    for(i=0;i<n;i++)
+    {
+        tat[i]=wt[i]+bt[i];
+        printf("P%d\t%d\t%d\n",i+1,wt[i],tat[i]);
+    }
+
+    return 0;
+}
